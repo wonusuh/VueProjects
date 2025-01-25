b008c137e8a0f65140dd8e446402011b5bf1669c
Wonu Suh <wonwooseo23@gmail.com>
2025-01-25 15:25:43 +0900

    modified : what is Vuex

--- vuestagram/src/App.vue
+++ vuestagram/src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="header">
-    <ul class="header-button-left">
+    <ul class=" header-button-left">
       <li>Cancel</li>
     </ul>
-    <ul class="header-button-right">
-      <li>Next</li>
-    </ul>
     <img src="./assets/logo.png" class="logo" alt="이미지" />
+    <ul class="header-button-right">
+      <li v-if="this.step === 2" v-on:click="publishAPost">Publish</li>
+      <li v-else v-on:click="moveToNext">Next</li>
+    </ul>
   </div>
 
-  <VuestaContainer v-bind:vuestaData="vuestaData" v-bind:step="step"></VuestaContainer>
+  <h4>안녕 {{ $store.state.name }}</h4>
+  <button v-on:click="$store.state.name = '박'">버튼</button>
+
+  <VuestaContainer v-bind:vuestaData="vuestaData" v-bind:step="step" v-bind:imgUrl="imgUrl"
+    v-bind:selectedFilter="selectedFilter" v-on:textChanged="handleTextChanged"></VuestaContainer>
   <button v-on:click="more">더보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
+      <input type="file" id="file" class="inputfile" v-on:change="upload" multiple accept="image/*" />
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
@@ -31,11 +36,21 @@ export default {
     return {
       vuestaData: vuestaData,
       indexForGetRequest: 0,
-      step: 0 // 현재 보역지고있는 페이지
+      step: 0, // 현재 보여지고있는 페이지의 인덱스
+      imgUrl: '', // 업로드한 이미지의 url
+      postContent: '',
+      selectedFilter: ''
     }
   },
+  mounted() {
+    /** 필터 적용 */
+    this.emitter.on('fire', (_selectedFilter) => {
+      console.log('at App', _selectedFilter)
+      this.selectedFilter = _selectedFilter
+    })
+  },
   methods: {
-    /**  get 요청 */
+    /** get 요청 */
     more() {
       axios.get(`https://codingapple1.github.io/vue/more${this.indexForGetRequest}.json`)
         .then((response) => {
@@ -48,13 +63,46 @@ export default {
         });
     },
 
-    /** 페이지 변경 이벤트 */
-    onClickChangeStep() {
+    /** 이미지 업로드 */
+    upload(event) {
+      const file = event.target.files
+      console.log('file -> ', file[0])
+      console.log('file.type -> ', file[0].type)
+      this.imgUrl = URL.createObjectURL(file[0])
+      console.log(this.imgUrl)
+      this.step = 1
+    },
 
+    /** 다음 페이지로 이동 */
+    moveToNext() {
+      this.step += 1
+      if (this.step > 2) this.step = 0
+    },
+
+    /** 글 하나 출판 */
+    publishAPost() {
+      const myPost = {
+        name: "Minny",
+        userImage: "https://picsum.photos/100?random=5",
+        postImage: this.imgUrl,
+        likes: 49,
+        date: "Apr 4",
+        liked: false,
+        content: this.postContent,
+        filter: this.selectedFilter,
+      }
+      console.log('myPost -> ', myPost)
+      this.vuestaData.unshift(myPost) // 배열의 첫 번째에 집어넣기
+      this.step = 0
+    },
+
+    /** 자식컴포넌트의 텍스트에어리어에서 받아온 글을 메인에 세팅 */
+    handleTextChanged(emittedValue) {
+      this.postContent = emittedValue
     }
   },
   components: {
-    VuestaContainer: VuestaContainer
+    VuestaContainer: VuestaContainer,
   }
 };
 </script>
--- vuestagram/src/components/VuestaContainer.vue
+++ vuestagram/src/components/VuestaContainer.vue
@@ -1,39 +1,58 @@
 <template>
     <!-- 포스트 -->
-    <VuestaPost v-if="step === 0" v-bind:vuestaData="vuestaData"></VuestaPost>
+    <VuestaPost v-if="step === 0" v-bind:vuestaData="vuestaData" v-bind:selectedFilter="selectedFilter"></VuestaPost>
 
     <!-- 필터선택페이지 -->
     <div v-if="step === 1">
-        <div class="upload-image"></div>
-        <div class="filters">
-            <div class="filter-1"></div>
-            <div class="filter-1"></div>
-            <div class="filter-1"></div>
-            <div class="filter-1"></div>
-            <div class="filter-1"></div>
+        <div v-bind:class="`upload-image ${this.selectedFilter}`" v-bind:style="`background-image: url(${imgUrl})`">
+        </div>
+        <div class=" filters">
+            <FilterBox v-bind:imgUrl="imgUrl" v-bind:filterClass="filtersList[i]" v-for="(filter, i) in filtersList"
+                v-bind:key="i">
+                {{ filter }}
+            </FilterBox>
         </div>
     </div>
 
     <!-- 글작성페이지 -->
     <div v-if="step === 2">
-        <div class="upload-image"></div>
+        <div v-bind:class="`upload-image ${selectedFilter}`" v-bind:style="{ backgroundImage: `url(${imgUrl})` }"></div>
         <div class="write">
-            <textarea class="write-box">write!</textarea>
+            <textarea class="write-box" v-on:input="onTextAreaChanged">write!</textarea>
         </div>
     </div>
 </template>
 
 <script>
 import VuestaPost from './VuestaPost.vue';
+import FilterBox from './FilterBox.vue';
 
 export default {
     name: "VuestaContainer",
+    emits: ['textChanged'],
+    data() {
+        return {
+            filtersList: ["aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",
+                "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua",
+                "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"],
+        }
+    },
     components: {
-        VuestaPost
+        VuestaPost,
+        FilterBox: FilterBox
     },
     props: {
         vuestaData: Array,
-        step: Number
+        step: Number,
+        imgUrl: String,
+        selectedFilter: String
+    },
+    methods: {
+        /** 텍스트에어리의 밸류가 변경될 때 부모컴포넌트로 보내기  */
+        onTextAreaChanged(event) {
+            console.log(event.target.value)
+            this.$emit('textChanged', event.target.value)
+        }
     }
 }
 </script>
